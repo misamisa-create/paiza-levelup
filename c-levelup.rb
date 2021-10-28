33570ed7e7842fa01a35aba84c3524f2d50981f4
misamisa-create <missmis0329@gmail.com>
2021-10-28 08:28:33 +0000

    [add]

---
+++ c-levelup.rb
@@ -0,0 +1,38 @@
+# 5以上の整数の合計
+
+# インクリメント
+number = gets.to_i
+puts number+1
+
+# 2.複数行の入力と出力
+number = gets.to_i
+number.times do
+  n = gets.to_i
+  puts n
+end
+
+# 3.配列（リスト）要素の合計 (paizaランク D 相当)
+array=[1, 3, 5, 6, 3, 2, 5, 23, 2]
+puts array.inject(:+)
+
+# 4.5以上か4以下か (paizaランク D 相当)
+num = gets.to_i
+if num >= 5
+  puts "high"
+else 
+  puts "low"
+end
+
+# 5.配列（リスト）データの足し合わせ (paizaランク D 相当)
+array=[4, 0, 5, -1, 3, 10, 6, -8]
+array= array.select{|n|n >=5 }
+puts array.inject(:+)
+
+# FINAL.5以上の整数の合計 (paizaランク C 相当)
+number = gets.to_i
+number2 = []
+number.times do
+  number2 << gets.to_i
+end
+number2 = number2.select{|n|n >= 5}
+puts number2.inject(:+)
