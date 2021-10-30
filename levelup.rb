f258a8fc5be2eb1485881da4649db90a3b549d12
misamisa-create <missmis0329@gmail.com>
2021-10-30 03:37:56 +0000

    [add]

--- levelup.rb
+++ levelup.rb
@@ -1,13 +1,6 @@
 # 五目並べ(ランクB)
 
 # 2.五目並べ(1行) (paizaランク D 相当)
-# 1行5列の五目並べの盤面が与えられます。
-# 盤面の各マスには、"O"か"X"か"."が書かれています。
-# "O"と"X"は、それぞれプレイヤーの記号を表します。
-# 同じ記号が横に連続で5つ並んでいれば、その記号のプレイヤーが勝者となります。
-# 勝者の記号を1行で表示してください。
-# 勝者がいない場合は、引き分けとして、"D"を表示してください。
-
 # array = %w[O X]
 # string = gets.chomp.split('')
 # result = 'D'
@@ -19,17 +12,9 @@
 #   string.each { |s| cnt = cnt + 1 if s == a }
 #   result = a if cnt >= 5
 # end
-
 # puts result
 
 # 3.五目並べ(横) (paizaランク C 相当)
-# 5行5列の五目並べの盤面が与えられます。
-# 盤面の各マスには、"O"か"X"か"."が書かれています。
-# "O"と"X"は、それぞれプレイヤーの記号を表します。
-# 同じ記号が横に連続で5つ並んでいれば、その記号のプレイヤーが勝者となります。
-# 勝者の記号を1行で表示してください。
-# 勝者がいない場合は、引き分けとして、"D"を表示してください。
-
 # array = %w[O X]
 # result = 'D'
 # 5.times do
@@ -46,22 +31,73 @@
 # puts result
 
 # 4.五目並べ（縦） (paizaランク C 相当)
+# board = []
+# result = 'D'
 
+# # 盤面の初期化
+# # 5回入力された内容をboardへpushする
+# (1..5).each { board.push(gets.chomp.split('')) }
+# 5パターン
+# (0..4).each do |i|
+#   # o,xが0個並んだ状態
+#   o = 0
+#   x = 0
+#   # 入力された内容を展開
+#   board.each do |row|
+#     # 展開された文字のi番目が対象の文字Oだった時
+#     if row[i] == 'O'
+#       # oの並ぶ数が１つ増える
+#       o = o + 1
+#     elsif row[i] == 'X'
+#       x = x + 1
+#     end
+#   end
+#   # もしoが5個ならんだら
+#   if o == 5
+#     result = 'O'
+#     break
+#   elsif x == 5
+#     result = 'X'
+#     break
+#   end
+# end
+# puts result
+
+# 5.五目並べ（斜め） (paizaランク C 相当)
+# 斜めで記号が揃うパターンは、0 から 4 までの i に対して board[i][i] が同じ記号か、board[i][4 - i] が同じ記号の 2 パターンです。
+# board[i][i] が同じ記号なのは左上から右下まで一直線に揃うパターン、board[i][4 - i] が同じ記号なのは右上から左下まで一直線に揃うパターンです。
 board = []
 result = 'D'
-
 # 盤面の初期化
-# 5回入力された内容をboardへpushする
 (1..5).each { board.push(gets.chomp.split('')) }
-
-(0..4).each do |i|
+# 2パターン
+(1..2).each do |time|
   o = 0
   x = 0
-  board.each do |row|
-    if row[i] == 'O'
+
+  i = 0
+
+  if time == 1
+    j = 0
+  else
+    j = 4
+  end
+
+  (1..5).each do
+    if board[i][j] == 'O'
       o = o + 1
-    elsif row[i] == 'X'
+    elsif board[i][j] == 'X'
       x = x + 1
+    else
+      break
+    end
+
+    i = i + 1
+
+    if time == 1
+      j = j + 1
+    else
+      j = j - 1
     end
   end
 
